7ef8e51c5cbb870dc072d901431e3f6912fe0525
umbertolia <bertik@sfr.fr>
2020-03-03 18:34:43 +0100

    V1

---
+++ appliAngularNoSql/src/main/java/spring/project/appliAngularNoSql/AppliAngularNoSqlApplication.java
@@ -0,0 +1,13 @@
+package spring.project.appliAngularNoSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliAngularNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliAngularNoSqlApplication.class, args);
+	}
+
+}
